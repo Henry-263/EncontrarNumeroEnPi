9ac63327adc0788f6d1dbd71ec16767e943d57ff
Henry <henrysanchezjuan@gmail.com>
2025-09-08 00:06:09 +0200

    Pasamos todo el proyecto a tkinter

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
+import tkinter as tk
+from tkinter import ttk
+
 with open("pi.txt", "r") as f:
-    pi_str = f.read().strip()  # Quita espacios al principio o final
+    pi_str = f.read().strip()
 
-num = input("Ingresa un número: ")
+def validate_entry(text):
+    return text.isdecimal()
+def obtener_numero(event = None):
+    num = texto.get()
+    texto.delete(0, tk.END)
+    pi_decimals = pi_str[2:] if pi_str.startswith("3.") else pi_str
 
-# Eliminamos el 3 y el punto inicial si es que están en pi.txt
-pi_decimals = pi_str[2:] if pi_str.startswith("3.") else pi_str
+    pos = pi_decimals.find(num)
+    if not num:
+        print("El numero ingresado no es valido")
+        resultado.config(text = "El numero ingresado no es valido")
+    elif pos == -1:
+        print("No existe el número en los primeros 100000 decimales de pi")
+        resultado.config(text = "No existe el número en los primeros 100000 decimales de pi")
+    else:
+        inicio = pos + 1
+        fin = pos + len(num)
+        print(f"El número está en la posición: ({inicio}-{fin})")
+        resultado.config(text = f"El número {num} está en la posición: ({inicio}-{fin})")
 
-pos = pi_decimals.find(num)
-if pos == -1:
-    print("No existe el número en los primeros 100000 decimales de pi")
-else:
-    # Posición humana (1 = primer decimal)
-    start = pos + 1
-    end = pos + len(num)
-    print(f"El número está en la posición: ({start}-{end})")
+
+
+ventana = tk.Tk()
+ventana.title("Numero dentro de pi")
+ventana.geometry("800x600")
+
+texto = tk.Entry(validate="key",
+    validatecommand=(ventana.register(validate_entry), "%S")
+)
+texto.place(x=300, y=50)
+
+resultado = tk.Label(text="")
+resultado.place(x=300, y=80)
+texto.bind("<Return>", obtener_numero)
+ventana.mainloop()
+
